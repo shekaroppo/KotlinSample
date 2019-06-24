184f5fa2c907d75c7309bac254439743e64269a6
Shekar <chandrashekar.allam@invia.de>
2019-06-24 17:00:25 +0200

    clean

--- app/src/main/java/com/shekar/kotlinsample/kotlinlang/Idioms.kt
+++ app/src/main/java/com/shekar/kotlinsample/kotlinlang/Idioms.kt
@@ -1,2 +1,14 @@
 package com.shekar.kotlinsample.kotlinlang
 
+data class Sum( val x: Int){
+
+   operator fun plus( sum : Sum): Int{
+        val s= sum.x * x
+        return s
+    }
+}
+
+fun main() {
+    val value = Sum(2) + Sum(2)
+    println(value)
+}
